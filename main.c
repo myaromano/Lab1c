8679bbb88a4e99027839d4a6142b3f756b20e873
myaromano <mlr5819@psu.edu>
2020-09-01 13:29:09 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,6 @@
+// Author: Mya Romano mlr5819@psu.edu
+// Collaborator: Octavia Szkutnik oas5135@psu.edu
+// Collaborator: Daniel Mikita djm6907@psu.edu
+// Collaborator: Joseph Imperial jri5073@psu.edu
+#include
+temperature = input("enter temperature in celsius: ")
