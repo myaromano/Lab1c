3106c96826d4ae06ecc3ff50d69d328dc2fb3254
myaromano <mlr5819@psu.edu>
2020-09-30 22:32:35 +0000

    added conversion equation

--- main.c
+++ main.c
@@ -2,5 +2,21 @@
 // Collaborator: Octavia Szkutnik oas5135@psu.edu
 // Collaborator: Daniel Mikita djm6907@psu.edu
 // Collaborator: Joseph Imperial jri5073@psu.edu
-#include
-temperature = input("enter temperature in celsius: ")
+#include <stdio.h>
+#include <readline/readline.h>
+#include <stdlib.h>
+
+int main(void) {
+  char *ctemp = readline("Enter temperature in Celsius: "); 
+
+  double c = atof(ctemp);
+  double f = atof(ctemp);
+
+  f = (c * 9/5) + 32;
+
+  printf("%.6f° celsius is equivilent to %f° Fahrenheit.\n",c, f);
+
+
+
+  return 0;
+}
